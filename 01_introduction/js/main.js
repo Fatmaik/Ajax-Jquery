5367acaf9e0e6fde69fd419328a0ee43f182242e
Dionathan <dionathan_bass@hotmail.com>
2017-03-28 15:09:44 -0300

    serialize done

--- 01_introduction/js/main.js
+++ 01_introduction/js/main.js
@@ -1,7 +1,21 @@
 $(function(){
     $('#AjaxRequest').submit(function(){
-        //ajax
-        alert("ok");
+        // recebe todos os campos do form
+        var form = $(this).serialize();
+
+        // variavel que recebe os valores por Aja
+        var request = $.ajax({
+            method:"POST",
+            url:"post.php",
+            // data receve os valores do form com jquery e armazena 
+            // nas variaveis do php 
+            data:form
+        });
+        request.always(function(e) {
+            console.log(e);
+        });
+
+        // retornar false impede qua a pagina atualize sosinha
         return false;
     });
 });
